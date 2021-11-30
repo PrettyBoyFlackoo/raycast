4e94789db7ba5a5c50d7779c3115992dc1391686
PrettyBoyFlackoo <95177386+PrettyBoyFlackoo@users.noreply.github.com>
2021-11-30 20:53:42 +0100

    Update Ray.hx

--- raycast/Ray.hx
+++ raycast/Ray.hx
@@ -47,7 +47,7 @@ class Ray {
             if (i != null) hits.push(i);
         }
 
-        if (hits.length > 0) {
+        if (hits.length > 1) {
             var nearestPoint0 = hits[0].distance(vFrom);
             var nearestPoint1 = hits[1].distance(vFrom);
     
@@ -56,6 +56,8 @@ class Ray {
             } else {
                 hit = hits[1];
             }
+        } else {
+            hit = hits[0];
         }
 
         return hit;
